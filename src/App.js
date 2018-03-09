79238d71990ce801f6691c51d0b3d8fa745aa5a5
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-03-08 21:51:17 -0500

    oh hell no

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  render () {
-    return (
-      <div>
-        Hello, App
-      </div>
-    )
+  render() {
+    return <div>Hello, App</div>
   }
 }
 
