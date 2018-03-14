5661a2427a79a4d8087d90281336466eeaab7314
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-03-13 22:22:26 -0400

    Fetch genes upon app mount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: false,
+      genes: []
+    }
+  }
+
+  async componentDidMount() {
+    this.setState({ isLoading: true })
+    const response = await fetch("/all_genes.json")
+    const allGenes = await response.json()
+    this.setState({ isLoading: false, genes: allGenes })
+  }
+
   render() {
-    return <div>Hello, App</div>
+    return (
+      <div>
+        {this.state.isLoading
+          ? "loading…"
+          : `${this.state.genes.length} genes loaded`}
+      </div>
+    )
   }
 }
 
